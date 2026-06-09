1e4291df3127c2471b415454ed9e7a19c8fc7fee
Ufuk <ufuk1821@gmail.com>
2026-06-09 04:21:30 +0300

    Fix init import and update project files

--- UfukMSA/__init__.py
+++ UfukMSA/__init__.py
@@ -1,4 +1,4 @@
-from UfukMSA.mafft import mafft, nw_align, pairwise_score
+from UfukMSA.mafft import mafft, nw_align, fft_similarity
 
 __version__ = "0.1.0"
 __all__ = ["mafft", "nw_align", "pairwise_score"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="UfukMSA-221201022",
-    version="0.2.0",
+    version="0.3.0",
     author="Ufuk Dogan",
     author_email="221201022@rumeli.edu.tr",
     description="MAFFT-based Multiple Sequence Alignment",
